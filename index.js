967d8e060ff4f5c920a4ad10559b1a4a9a5ada8f
Haeven Kelley <haeven@compknowhow.com>
2019-07-12 10:38:05 -0500

    added remaining files

---
+++ index.js
@@ -0,0 +1,79 @@
+const request = require('request');
+const util = require('util');
+const winston = require('winston');
+
+/**
+ * Default formatting for messages sent to MSTeams
+ * @param {string} Logging level
+ * @param {string} Message to send to MSTeams
+ */
+function defaultFormatter(level, message) {
+	return ['[', level, ']', ' ', message].join('');
+  }
+
+/**
+ * MSTeams integration for Winston
+ * @param {object} Options parameter
+ */
+function MSTeams(options) {
+  if (!options.webhook || typeof options.webhook !== 'string') {
+    throw new Error('Invalid webhook parameter');
+  }
+
+  this.name = options.name;
+  this.webhook = options.webhook;
+  delete options.webhook;
+  this.options = {
+	"webhook": options.webhook,
+	"username": options.username,
+	"level": options.level,
+	"summary": options.summary,
+	"themeColor": options.themeColor,
+	"sections": [{
+        "activityTitle": options.activityTitle,
+        "activitySubtitle": options.activitySubtitle,
+		"activityImage": options.activityImage,
+    }]
+  };
+}
+
+/**
+ * Handles the sending of a message to an Incoming webhook
+ * @param {text} Message text
+ * @param {function} Callback function for post execution
+ */
+function send(message, callback) {
+  if (!message) {
+    return callback(new Error('No message'));
+  }
+
+  this.options.sections[0].text = JSON.stringify(message);
+  const requestParams = {
+    url: this.webhook,
+    body: this.options,
+    json: true
+  };
+
+  return request.post(requestParams, function (err, res, body) {
+    if (err || body !== 'ok') {
+      return callback(err || new Error(body));
+    }
+    return callback(null, body);
+  });
+}
+
+util.inherits(MSTeams, winston.Transport);
+winston.transports.MSTeams = MSTeams;
+
+/**
+ * Log method for Winston integration
+ * @param {string} Logging level
+ * @param {string} Message to send to MSTeams
+ * @param {string} Meta data for styling
+ * @param {function} Callback function for post execution
+ */
+MSTeams.prototype.log = function (level, message, meta, callback) {
+  return send.call(this, defaultFormatter(level, message, meta), callback);
+};
+
+module.exports = MSTeams;
